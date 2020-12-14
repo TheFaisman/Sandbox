c1220abf290f213e60b4d0069b9424d13ab63f9c
Faisal Naqaweh <f.naqaweh@gmail.com>
2020-12-13 22:30:29 -0500

    Adding the Makefile

--- Word_Filter/word_filter.cpp
+++ Word_Filter/word_filter.cpp
@@ -2,6 +2,11 @@
 #include <iostream>
 #include <string>
 #include <vector>
+/*
+    This code accepts a list of words from the user and prints an ordered
+    version of this list. Repetitive words along with predetermined disliked
+    words are omitted from the printed list. 
+*/
 
 int main()
 {
