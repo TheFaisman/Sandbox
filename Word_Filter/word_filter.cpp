09f80bb58a1db41e145047f5285fc253efb36739
Faisal Naqaweh <f.naqaweh@gmail.com>
2021-06-20 12:08:13 +0400

    small edits and refactoring

--- Word_Filter/word_filter.cpp
+++ Word_Filter/word_filter.cpp
@@ -4,40 +4,43 @@
 #include <vector>
 /*
     This code accepts a list of words from the user and prints an ordered
-    version of this list. Repetitive words along with predetermined disliked
+    version of this list. Repetitive words along with predetermined censored
     words are omitted from the printed list. 
 */
 
 int main()
 {
-    // List of unacceptable words
+    // List of words to be censored
     std::vector<std::string> disliked { "fruit", "cars", "bad" };
 
     std::cout << "Provide a list of words.\n";
 
     std::vector<std::string> dictionary;
     for (std::string word; std::cin >> word;) {
-        dictionary.push_back(word);
+        if (word == "end")
+            break;
+        else
+            dictionary.push_back(word);
     }
 
     std::sort(dictionary.begin(), dictionary.end());
 
     std::cout << "\nHere is the sorted dictionary.\n";
 
-    bool dirty_word = false;
+    bool censored_word = false;
 
     for (int i = 0; i < dictionary.size(); i++) {
         for (int j = 0; j < disliked.size(); j++) {
-            if (dictionary[i] == disliked[j]) // Found a dirty word
-                dirty_word = true;
+            if (dictionary[i] == disliked[j]) // Found a forbidden word
+                censored_word = true;
         }
-        if (dirty_word) {
-            std::cout << "BLEEP" << std::endl;
+        if (censored_word) {
+            std::cout << "*****" << std::endl;
         } else if (i == 0 || dictionary[i - 1] != dictionary[i]) { // Ensure no repetition
 
             std::cout << dictionary[i] << std::endl;
         }
-        dirty_word = false;
+        censored_word = false;
     }
 
     return 0;
