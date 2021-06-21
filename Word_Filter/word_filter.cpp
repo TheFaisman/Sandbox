bbc6d0b60e4716d47c55f8af0fd7f9f9aaf6c062
Faisal Naqaweh <f.naqaweh@gmail.com>
2021-06-21 11:03:38 +0400

    The dictionary string vector now only contains unique instances of user input. Abstracted vector printing into its own function.

--- Word_Filter/word_filter.cpp
+++ Word_Filter/word_filter.cpp
@@ -3,7 +3,31 @@
     This code accepts a list of words from the user and prints an ordered
     version of this list. Repetitive words along with predetermined censored
     words are omitted from the printed list. 
+
+    There are several ways to implement this.
+    1. We can push all words into the "dictionary" vector, then only print the unique ones
+    2. We can iterate through the "dictionary" vector to ensure we only store unique words into it,
+        then print the "dictionary" vector
+    3. We can push all words into the "dictionary" vector, then push unique words into a
+        "unique_dictionary" vector, and print the "unique_dictionary" vector
+
+    What are the benefits and setbacks of each method?
+    Method 1:
+        One loop iteration the size of the dictionary, and minimal memory usage. The problem is that 
+        iteration over the repeated words is required every time in order to print unique words
+    Method 2 & 3:
+        Two loop iterations; one to store all words in vector A, and another to store the unique words
+        of vector A into vector B. For future lookups or prints, iteration over repeated words (vector A)
+        is not required. The setback is that additional memory is used, however vector A can be discarded.
+    
+    In the interest of maintainability and future interest in unique words only, Method 3 will be used.
 */
+
+void PrintVector(vector<string> v) {
+    for (int i = 0; i < v.size(); i++) {
+        cout << v[i] << endl;
+    }
+}
 
 int main()
 {
@@ -13,34 +37,44 @@ int main()
     disliked.push_back("bad");
     disliked.push_back("car");
 
-    cout << "Provide a list of words. Terminate the list with an end of file character\n";
+    cout << "Provide a list of words. Terminate the list with an end of file character (Ctrl+Z).\n";
 
-    vector<string> dictionary;
+    vector<string> input_strings;
+    // Collect all the input into a temporary vector
     for (string word; cin >> word;) {
-        // Ctrl+Z to break the loop
-        dictionary.push_back(word);
+        input_strings.push_back(word);
     }
 
-    sort(dictionary.begin(), dictionary.end());
+    // Sort the input string vector to cause repeated words to be adjacent to each other
+    sort(input_strings.begin(), input_strings.end());
+
+    // Create a new string vector to hold unique input string values
+    vector<string> dictionary;
+    for (int i = 0; i < input_strings.size(); i++) {
+        if (i == 0 || input_strings[i - 1] != input_strings[i]) {
+            dictionary.push_back(input_strings[i]);
+        }
+    }
+
+    // Discard the string vector containing repeated words
+    input_strings.clear();
 
     cout << "\nHere is the sorted dictionary.\n";
 
-    bool censored_word = false;
+    bool is_word_censored = false;
+    string censored_string = "*****";
 
     for (int i = 0; i < dictionary.size(); i++) {
         for (int j = 0; j < disliked.size(); j++) {
             if (dictionary[i] == disliked[j]) // Found a forbidden word
-                censored_word = true;
+                is_word_censored = true;
         }
-        if (i == 0 || dictionary[i - 1] != dictionary[i]) { // Ensure no repetition
-            if (censored_word)
-                cout << "*****" << endl;
-            else 
-                cout << dictionary[i] << endl;
-            censored_word = false;
-        }
-        
+        if (is_word_censored)
+            dictionary[i] = censored_string;
+        is_word_censored = false;
     }
+
+    PrintVector(dictionary);
 
     return 0;
 }
