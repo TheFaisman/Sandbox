e1836f5c1f7dbc66f184fe68df19923d0155dc81
Faisal Naqaweh <f.naqaweh@gmail.com>
2021-06-24 10:08:46 +0400

    Adding exercises from the end of chapter 3.

--- Chapter_3/exercise_3_7.cpp
+++ Chapter_3/exercise_3_7.cpp
@@ -1,20 +1,8 @@
 #include "../std_lib_facilities.h"
-/*
-    This program accepts three integer inputs then outputs them in numerical
-    sequence in a comma separated format.
-
-    No iteration is used, there's likely room to improve this program.
-*/
-template <class T>
-void Swap(T &first, T &second) {
-    T temp = first;
-    first = second;
-    second = temp;
-}
 
 int main() {
-    int input[3];
-    cout << "Enter 3 integers: ";
+    string input[3];
+    cout << "Enter 3 strings: ";
     for (int i = 0; i < 3; i++) {
         cin >> input[i];
     }
