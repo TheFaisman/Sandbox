7bd7857b71d4a89aaa644a8a3a66144efe42e57a
Faisal Naqaweh <f.naqaweh@gmail.com>
2021-06-29 13:45:04 +0400

    Simple calculator program.

--- Chapter_4/exercise_4_5.cpp
+++ Chapter_4/exercise_4_5.cpp
@@ -7,4 +7,41 @@
     values and a character to represent an operation.
 
     For eg. The input '53.1 34.3 +' should output "The sum of 43.1 and 34.3 is 87.4."
-*/  
+*/
+
+int main() {
+    cout << endl;
+    cout << "Simple Calculator 1.0 - Est. 2021\n";
+    cout << "Supported operations include +, -, /, *.\n";
+    cout << "Input example: '2 3 +'\n" << endl;
+    cout << "Input: ";
+
+    double input_1, input_2, result = 0;
+    char operation = ' ';
+
+    cin >> input_1 >> input_2 >> operation;
+
+    switch (operation)
+    {
+    case '+':
+        result = input_1 + input_2;
+        break;
+    case '-':
+        result = input_1 - input_2;
+        break;
+    case '/':
+        result = input_1 / input_2;
+        break;
+    case '*':
+        result = input_1 * input_2;
+        break;
+    default:
+        cerr << "Operation undefined. I do not support the '" << operation << "' operator.\n";
+        simple_error("Operator input error.");
+        break;
+    }
+
+    cout << "The output is " << result << ".\n";
+
+    return 0;
+}
