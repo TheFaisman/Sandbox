12d532a83f1a318e77dbbcaaeb84791dde264266
Faisal Naqaweh <f.naqaweh@gmail.com>
2021-06-29 09:46:07 +0400

    Program that determines the mode of a positive set of integers.

--- Chapter_4/exercise_4_16.cpp
+++ Chapter_4/exercise_4_16.cpp
@@ -4,12 +4,22 @@
 */
 template <class T>
 T find_mode(vector<T> arg_vector) {
-    T current_mode;
+    T current_mode = 0;
     T num_of_occurences = 0;
+    T max_occurences = 0;
     for (int i = 0; i < arg_vector.size(); i++) {
-
+        for (int j = i; j < arg_vector.size(); j++) {
+            if (arg_vector[i] == arg_vector[j]) {
+                num_of_occurences++;
+            }
+        }
+        if (num_of_occurences > max_occurences) {
+            max_occurences = num_of_occurences;
+            current_mode = arg_vector[i];
+        }
+        num_of_occurences = 0;
     }
-    return T;
+    return current_mode;
 }
 
 int main() {
@@ -18,8 +28,6 @@ int main() {
     for (int temp; cin >> temp;) {
         input_values.push_back(temp);
     }
-    
-    sort(input_values);
 
     int mode = find_mode(input_values);
     cout << "The mode of the set of positive integers is " << mode << ".\n";
